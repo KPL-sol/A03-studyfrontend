8e31fe3d9ebe65c2902faa13bff6da5d2e3c98ec
Kanawat <kanawatpumchai@gmaol.com>
2026-03-11 22:43:06 +0700

    Pass test case

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,11 +1,20 @@
+import Image from 'next/image';
 import styles from './banner.module.css';
 
 export default function Banner() {
     return (
         <div className={styles.banner}>
+            <Image 
+                src="/img/cover.png" 
+                alt="Banner Image" 
+                fill={true} 
+                priority
+                className={styles.bannerImg}
+            />
+            
             <div className={styles.bannerText}>
                 <h1>where every event finds its venue</h1>
-                <p>สัมผัสประสบการณ์การจัดงานที่เหนือระดับ ด้วยสถานที่ที่ตอบโจทย์ทุกความต้องการของคุณ</p>
+                <p>สัมผัสประสบการณ์การจัดงานที่เหนือระดับ</p>
             </div>
         </div>
     );
